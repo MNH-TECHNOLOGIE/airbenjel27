0a60b035e633bc0af58542cf1835b98d12b5d640
Palmador Developer <mohamed12sar@gmail.com>
2026-02-26 05:43:33 +0000

    Skip ESLint during build on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["localhost"],
     remotePatterns: [
@@ -20,4 +23,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
